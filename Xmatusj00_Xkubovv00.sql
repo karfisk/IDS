3ee9bd539d655fc1da625b3b2b1ef3b12a4d40b3
Veronika Kubová <root@PC-Veru>
2025-04-13 20:56:42 +0200

    added 2 SELECTs, edited format of comments, addded some more data, fixed one SELECT that wasn't working and modified it a bit (Which orders cost more than 200Kc?), also fixed PK of is_a_part_of - was messing stuff up

--- Xmatusj00_Xkubovv00.sql
+++ Xmatusj00_Xkubovv00.sql
@@ -23,6 +23,8 @@ MAXVALUE 50
 NOCACHE
 NOCYCLE; 
 
+---------------------------- Table definitions ----------------------------
+
 -- Entity User generalizes the "Employee" and "Customer" entities. 
 -- These entities are the only possible specializations and when creating reservations or orders, there is no difference between them. 
 -- Therefore, the generalization/specialization is implemented as a single shared table with a distinguishing atribute "user_position".
@@ -34,43 +36,49 @@ CREATE TABLE rest_user (
     user_position VARCHAR2(20) NOT NULL, 
     user_adress VARCHAR2(255),
     CONSTRAINT pk_user PRIMARY KEY (user_id),
-    CONSTRAINT chk_email_format CHECK (REGEXP_LIKE(email_adress, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')), -- CHECKING FORMAT OF EMAIL ADRESS
-    CONSTRAINT chk_telephone_num CHECK (REGEXP_LIKE(telephone_num, '^0[0-9]{2,3} ?[0-9]{3} ?[0-9]{3}$')), -- CHECKING FORMAT OF TEL NUM
-    CONSTRAINT chk_position CHECK (user_position IN ('Employee', 'Customer')), -- ONLY EMPLOYEE AND CUSTOMER ALLOWED AS POSITION
+    CONSTRAINT chk_email_format CHECK (REGEXP_LIKE(email_adress, '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$')), -- checking format of email adress
+    CONSTRAINT chk_telephone_num CHECK (REGEXP_LIKE(telephone_num, '^0[0-9]{2,3} ?[0-9]{3} ?[0-9]{3}$')), -- checking format of tel num
+    CONSTRAINT chk_position CHECK (user_position IN ('Employee', 'Customer')), -- only employee and customer are allowed as position
     CONSTRAINT chk_adress CHECK (
         (user_position = 'Customer' AND user_adress IS NULL ) OR 
         (user_position = 'Employee' And user_adress IS NOT NULL)
     )
 );
 
+-- Reservation
 CREATE TABLE reservation (
     reservation_id NUMBER GENERATED ALWAYS AS IDENTITY,
-    reservation_holder VARCHAR2(100) NOT NULL,
+    reservation_holder VARCHAR2(100) NOT NULL,  -- may differ from the user who created a reservation
     user_id NUMBER NOT NULL,
     CONSTRAINT pk_reservation PRIMARY KEY (reservation_id),
-    CONSTRAINT fr_user_reserv FOREIGN KEY (user_id) REFERENCES rest_user(user_id) -- EVEN IF USER IS DELETED WE KEEP RECORD OF RESERVATION (EMPLOYEE CAN MAKE RESERVATION IN THE NAME OF CUSTOMER)
+    -- even if user is deleted, we keep record of reservations (employees are able to make reservations on behalf of customers)
+    CONSTRAINT fr_user_reserv FOREIGN KEY (user_id) REFERENCES rest_user(user_id)
 );
 
+-- Saloon
 CREATE TABLE rest_saloon (
     saloon_id NUMBER GENERATED ALWAYS AS IDENTITY,
     saloon_type VARCHAR2(50) NOT NULL,
     CONSTRAINT pk_saloon PRIMARY KEY (saloon_id)
 );
 
+-- Table
 CREATE TABLE rest_table (
     table_id NUMBER DEFAULT table_seq.NEXTVAL PRIMARY KEY,
     num_of_seats NUMBER NOT NULL,
     saloon_id NUMBER,
     CONSTRAINT fr_saloon_table FOREIGN KEY (saloon_id) REFERENCES rest_saloon(saloon_id), 
-    CONSTRAINT chk_num_of_seats CHECK (num_of_seats > 0) -- TABLE HAS AT LEAST ONE CHAIR
+    CONSTRAINT chk_num_of_seats CHECK (num_of_seats > 0) -- each table has at least one seat
 );
 
+-- Order
 CREATE TABLE rest_order (
     order_id NUMBER GENERATED ALWAYS AS IDENTITY,
     order_time TIMESTAMP NOT NULL,
     CONSTRAINT pk_order PRIMARY KEY (order_id)
 );
 
+-- Menu_item
 CREATE TABLE menu_item (
     menu_item_id NUMBER GENERATED ALWAYS AS IDENTITY,
     menu_item_name VARCHAR2(100) NOT NULL, 
@@ -79,6 +87,7 @@ CREATE TABLE menu_item (
     CONSTRAINT chk_item_price CHECK (menu_item_price > 0)
 );
 
+-- Ingredient
 CREATE TABLE ingredient (
     ingredient_id NUMBER GENERATED ALWAYS AS IDENTITY,
     ingredient_name VARCHAR2(100) NOT NULL,
@@ -88,14 +97,14 @@ CREATE TABLE ingredient (
     CONSTRAINT pk_ingredient PRIMARY KEY (ingredient_id)
 );
 
--- CHANGE FROM COMPOSIT ATRIBUTED TO NEW ENTITY  
+-- Alergen (changed from composite atribute to a new entity set)
 CREATE TABLE alergen (
     alergen_id NUMBER GENERATED ALWAYS AS IDENTITY,
     alergen_name VARCHAR2(100) NOT NULL,
     CONSTRAINT pk_alergen PRIMARY KEY (alergen_id)
 );
 
-
+-- Reservation for a saloon
 CREATE TABLE saloon_reservation (
     event_name VARCHAR2(100) NOT NULL,
     event_time TIMESTAMP NOT NULL,
@@ -107,6 +116,7 @@ CREATE TABLE saloon_reservation (
     CONSTRAINT fk_saloon_id FOREIGN KEY (saloon_id) REFERENCES rest_saloon(saloon_id) ON DELETE CASCADE
 );
 
+-- Reservation for a table
 CREATE TABLE table_reservation (
     event_time TIMESTAMP NOT NULL,
     reservation_id NUMBER NOT NULL,
@@ -116,27 +126,31 @@ CREATE TABLE table_reservation (
     CONSTRAINT fr_table_table FOREIGN KEY (table_id) REFERENCES rest_table(table_id) ON DELETE CASCADE
 );
 
+-- User makes an order for a table
 CREATE TABLE makes (
     user_id NUMBER NOT NULL,
     order_id NUMBER NOT NULL,
     table_id NUMBER NOT NULL,
     CONSTRAINT pk_makes PRIMARY KEY (user_id, order_id, table_id),
-    CONSTRAINT fr_user_makes FOREIGN KEY (user_id) REFERENCES rest_user(user_id), -- WE WANT TO KEEP RECORDS ON WHO CREATED ORDERS
-    CONSTRAINT fr_order_makes FOREIGN KEY (order_id) REFERENCES rest_order(order_id) ON DELETE CASCADE, -- THIS INFORMATION IS NOT IMPORTANT TO US
-    CONSTRAINT fr_table_makes FOREIGN KEY (table_id) REFERENCES rest_table(table_id) ON DELETE CASCADE  -- THIS INFORMATION IS NOT IMPORTANT TO US
+    CONSTRAINT fr_user_makes FOREIGN KEY (user_id) REFERENCES rest_user(user_id), -- we want to keep record of who created orders
+    CONSTRAINT fr_order_makes FOREIGN KEY (order_id) REFERENCES rest_order(order_id) ON DELETE CASCADE, -- this information is not important to us
+    CONSTRAINT fr_table_makes FOREIGN KEY (table_id) REFERENCES rest_table(table_id) ON DELETE CASCADE  -- this information is not important to us
 );
 
+-- Menu_item is a part of an order
 CREATE TABLE is_a_part_of (
+    is_a_part_of_id NUMBER GENERATED ALWAYS AS IDENTITY,
     quantity NUMBER NOT NULL,
     order_note VARCHAR2(200),
     menu_item_id NUMBER NOT NULL,
     order_id NUMBER NOT NULL,
-    CONSTRAINT pk_is_a_part_of PRIMARY KEY (menu_item_id, order_id),
-    CONSTRAINT fk_menu_item_part_of FOREIGN KEY (menu_item_id) REFERENCES menu_item(menu_item_id), -- TO PREVENT UNINTATIONAL REMOVAL OF INFORMATION ABOUT ORDER
+    CONSTRAINT pk_is_a_part_of PRIMARY KEY (is_a_part_of_id),
+    CONSTRAINT fk_menu_item_part_of FOREIGN KEY (menu_item_id) REFERENCES menu_item(menu_item_id), -- to prevent unintentional removal of information about order
     CONSTRAINT fk_order_part_of FOREIGN KEY (order_id) REFERENCES rest_order(order_id) ON DELETE CASCADE,
-    CONSTRAINT chk_quantity CHECK (quantity > 0) -- ORDER MUST CONTAIN AT LEAST ONE ITEM 
+    CONSTRAINT chk_quantity CHECK (quantity > 0) -- each order has at least one menu_item 
 );
 
+-- Menu_item consists of ingredients
 CREATE TABLE consists_of (
     menu_item_id NUMBER NOT NULL,
     ingredient_id NUMBER NOT NULL,
@@ -145,6 +159,7 @@ CREATE TABLE consists_of (
     CONSTRAINT fr_ingredient_consists_of FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id) ON DELETE CASCADE
 );
 
+-- Employee uses ingredients (takes them from storage) 
 CREATE TABLE uses (
     use_time TIMESTAMP NOT NULL,  
     ingredient_amount NUMBER NOT NULL,
@@ -155,6 +170,7 @@ CREATE TABLE uses (
     CONSTRAINT fk_ingredient_uses FOREIGN KEY (ingredient_id) REFERENCES ingredient(ingredient_id)
 );
 
+-- Ingredient can contain (or be) an alergen
 CREATE TABLE contains (
     ingredient_id NUMBER NOT NULL,
     alergen_id NUMBER NOT NULL,
@@ -163,9 +179,10 @@ CREATE TABLE contains (
     CONSTRAINT fr_alergen_contains FOREIGN KEY (alergen_id) REFERENCES alergen(alergen_id) ON DELETE CASCADE
 );
 
---TIME TO FILL TABLES WITH TEST VALUES
 
--- USER
+---------------------------- Filling test data ----------------------------
+
+-- User
 INSERT INTO rest_user (user_name, email_adress, telephone_num, user_position, user_adress)
 VALUES ('Obi-Wan Kenobi', 'HighGroundEnjoyer@gmail.com', '0954 123 451', 'Employee', 'Tatooine 20, Mos Eisley');
 INSERT INTO rest_user (user_name, email_adress, telephone_num, user_position, user_adress)
@@ -203,19 +220,19 @@ VALUES ('Luke Skywalker', 'JediLikeMyFather@gmail.com', '0445 642 461', 'Custome
 INSERT INTO rest_user (user_name, email_adress, telephone_num, user_position, user_adress)
 VALUES ('Kylo Ren', 'darthVader_fanclub@seznam.cz', '0882 436 332', 'Customer', NULL);
 
--- RESERVATION
-INSERT INTO reservation (reservation_holder, user_id) -- doplnat automaticky reserv holder?
+-- Reservation
+INSERT INTO reservation (reservation_holder, user_id)
 VALUES ('General Grievous', 1);
 INSERT INTO reservation (reservation_holder, user_id)
 VALUES ('General Grievous', 2);
 
--- SALOON
+-- Saloon
 INSERT INTO rest_saloon (saloon_type)
 VALUES ('Sand saloon');
 INSERT INTO rest_saloon (saloon_type)
 VALUES ('Smoking saloon');
 
--- TABLE
+-- Table
 INSERT INTO rest_table (num_of_seats, saloon_id)
 VALUES (4, 1);
 INSERT INTO rest_table (num_of_seats, saloon_id)
@@ -225,14 +242,26 @@ VALUES (3, 2);
 INSERT INTO rest_table (num_of_seats, saloon_id)
 VALUES (5, NULL);
 
--- ORDER
+-- Order
 INSERT INTO rest_order (order_time)
 VALUES (TO_TIMESTAMP('08.04.2025 18:15:00', 'DD.MM.YYYY HH24:MI:SS'));
 INSERT INTO rest_order (order_time)
 VALUES (TO_TIMESTAMP('08.04.2025 18:16:00', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('26.03.2025 13:46:25', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('13.04.2025 20:50:25', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('13.04.2025 20:50:43', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('08.02.2025 17:26:10', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('08.02.2025 17:26:43', 'DD.MM.YYYY HH24:MI:SS'));
+INSERT INTO rest_order (order_time)
+VALUES (TO_TIMESTAMP('08.02.2025 17:27:08', 'DD.MM.YYYY HH24:MI:SS'));
 
 
--- MENU ITEM
+-- Menu_item
 INSERT INTO menu_item (menu_item_name, menu_item_price)
 VALUES ('Cerveza Cristal', 500);
 INSERT INTO menu_item (menu_item_name, menu_item_price)
@@ -248,9 +277,9 @@ VALUES ('Iceberg Ice Tea', 25);
 INSERT INTO menu_item (menu_item_name, menu_item_price)
 VALUES ('Chilly Wine', 60);
 INSERT INTO menu_item (menu_item_name, menu_item_price)
-VALUES ('Angel''s Coffee', 45);
+VALUES ('Angels Coffee', 45);
 
--- INGREDIENT
+-- Ingredient
 INSERT INTO ingredient (ingredient_exper_date, ingredient_amount, ingredient_unit, ingredient_name) 
 VALUES (TO_DATE('4.9.2026', 'DD.MM.YYYY'), 0.5, 'l', 'CERVEZA CRISTAL');
 INSERT INTO ingredient (ingredient_exper_date, ingredient_amount, ingredient_unit, ingredient_name) 
@@ -286,7 +315,7 @@ VALUES (TO_DATE('21.6.2027', 'DD.MM.YYYY'), 10, 'l', 'Chilly Wine');
 INSERT INTO ingredient (ingredient_exper_date, ingredient_amount, ingredient_unit, ingredient_name)
 VALUES (TO_DATE('28.4.2027', 'DD.MM.YYYY'), 20, 'kg', 'Coffee beans');
 
--- ALERGEN
+-- Alergen
 INSERT INTO alergen (alergen_name)
 VALUES ('gluten');
 INSERT INTO alergen (alergen_name)
@@ -296,27 +325,65 @@ VALUES ('eggs');
 INSERT INTO alergen (alergen_name)
 VALUES ('milk');
 
--- SALOON RESERVATION
+-- Reservation for a saloon
 INSERT INTO saloon_reservation (event_name, event_time, event_note, reservation_id, saloon_id)
 VALUES ('Welcome party', TO_TIMESTAMP('10.04.2025 19:00:00', 'DD.MM.YYYY HH24:MI:SS'), NULL, 1, 1);
 
--- TABLE RESERVATION
+-- Reservation for a table
 INSERT INTO table_reservation (event_time, reservation_id, table_id)
 VALUES (TO_TIMESTAMP('08.04.2025 18:15:00', 'DD.MM.YYYY HH24:MI:SS'), 2, 4);
 
--- MAKES
-INSERT INTO makes (user_id,order_id, table_id)
+-- User makes an order for a table
+INSERT INTO makes (user_id, order_id, table_id)
 VALUES (2, 1, 4);
-INSERT INTO makes (user_id,order_id, table_id)
+INSERT INTO makes (user_id, order_id, table_id)
 VALUES (2, 2, 4);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (7, 3, 3);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (2, 4, 3);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (8, 5, 3);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (9, 6, 1);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (3, 7, 3);
+INSERT INTO makes (user_id, order_id, table_id)
+VALUES (1, 8, 3);
 
---IS A PART OF
+-- Menu_item is a part of an order
 INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
 VALUES (4, 'CERVEZA CRISTAL!!!', 1, 1);
 INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
-VALUES (4, 'I`m quite hungry', 2, 1);
+VALUES (4, 'Im quite hungry', 2, 1);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, NULL, 5, 2);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, 'Wesa goen underwater, okeyday?', 3, 3);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (2, 'Wesa goen underwater, okeyday?', 6, 3);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (3, 'Kenobiiii!', 7, 4);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, 'Kenobiiii!', 4, 4);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (4, 'KENOBIIIIII!', 7, 5);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (2, 'KENOBIIIIII!', 1, 5);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, 'Always fun when Skyguy comes by', 3, 6);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, 'Always fun when Skyguy comes by', 8, 6);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, NULL, 4, 7);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, 'Its like being on Hoth again', 6, 7);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, NULL, 2, 8);
+INSERT INTO is_a_part_of (quantity, order_note, menu_item_id, order_id)
+VALUES (1, NULL, 8, 8);
 
--- CONSITS OF
+-- Menu_item consists of ingredients
 INSERT INTO consists_of (menu_item_id, ingredient_id)
 VALUES (1, 1);
 INSERT INTO consists_of (menu_item_id, ingredient_id)
@@ -354,13 +421,13 @@ VALUES (8, 4);
 INSERT INTO consists_of (menu_item_id, ingredient_id)
 VALUES (8, 17);
 
--- USES
+-- Employee uses ingredients
 INSERT INTO uses (use_time, ingredient_amount, user_id, ingredient_id)
 VALUES (TO_TIMESTAMP('08.04.2025 18:18:00', 'DD.MM.YYYY HH24:MI:SS'), 4, 1, 1);
 INSERT INTO uses (use_time, ingredient_amount, user_id, ingredient_id)
 VALUES (TO_TIMESTAMP('08.04.2025 18:18:00', 'DD.MM.YYYY HH24:MI:SS'), 4, 1, 2);
 
--- CONTAINS
+-- Ingredient can contain (of be) an alergen
 INSERT INTO contains (ingredient_id, alergen_id)
 VALUES (2, 2);
 INSERT INTO contains (ingredient_id, alergen_id)
@@ -390,66 +457,67 @@ SELECT * FROM CONSISTS_OF;
 SELECT * FROM USES;
 SELECT * FROM CONTAINS;
 
+---------------------------- Querying the database ----------------------------
 
--- TODO - pridat dotazy na tabulky pomocou SELECT:
---
--- Mozne selecty? => kolko obiednavok/reservacii vytvoril user,
---                => kolko z obiednavok bolo vytvorenych zakaznikom a kolko zamestnancom, 
---                => kolko menu_items obsahuje alergen/ingerdienciu, 
---                => kolko ingrediencii obsahuju menu_item,
---                => kolko rezervacii je vytvorenych pre salonik a kolko pre stoly, 
---                => nieco s casom (napr kolko obiednavok bolo vytvorenych v tom a v tom case, pocet ingrediencii zobrany napriklad za mesiac,...),
---                => kolko sa predalo z roznych menu_items a ake menu_item sa predal najviac
---
--- Dalej skontroluj ci tieto SELECT splnaju poziadavky !!!!
--- A samozrejme implementuj.
-
--- What ingriedients are used in menu item (for example 'svieckova')?
--- This request is done using IN 
+-- Which ingriedients are used in menu item 'Ration Pack'?
+-- (IN predicate with a subquery)
 SELECT ingredient_name
 FROM ingredient 
 WHERE ingredient_id IN 
     (SELECT ingredient_id FROM consists_of
      WHERE menu_item_id IN
      (SELECT menu_item_id FROM menu_item
-     WHERE menu_item_name = 'Svieckova')); 
+     WHERE menu_item_name = 'Ration Pack')); 
 
--- HOW MANY INGREDIENTS ARE IN A MENU ITEM (LETS USE 'SVIECKOVA' ONCE AGAIN)?
-
+-- Which ingredients are in menu item 'Svieckova' and how many of each one?
+-- (JOIN of 3 tables)
 SELECT ing.ingredient_id, ing.ingredient_name, COUNT(*) AS num_of_ingredients
 FROM ingredient ing 
 JOIN consists_of c_f ON ing.ingredient_id = c_f.ingredient_id
 JOIN menu_item mn_it ON c_f.menu_item_id = mn_it.menu_item_id
 WHERE mn_it.menu_item_name = 'Svieckova' 
 GROUP BY ing.ingredient_id, ing.ingredient_name;
-
--- SHOW ONLY ORDERS THAT COST MORE THAN 100KC 
-
-SELECT rst_ord.order_id, SUM(is_a_part_of.quantity * menu_item.menu_item_price) as final_sum 
-FROM rest_order rst_ord
-JOIN is_a_part_of ON rst_ord.order_id = is_a_part_of.order_id
-JOIN menu_item ON is_a_part_of.menu_item_id = menu_item.menu_item_id
-GROUP BY rst_ord.order_id
-HAVING SUM(is_a_part_of.quantity * menu_item.menu_item_price) > 100;
  
--- HOW MANY ORDERS HAS GENERAL GRIEVOUS MADE
-
+-- Which orders cost more than 200Kc?
+-- (GROUP BY and an agregation function)
+SELECT ro.order_id, SUM(iap.quantity * m.menu_item_price) as final_sum 
+FROM rest_order ro
+LEFT JOIN is_a_part_of iap ON ro.order_id = iap.order_id
+LEFT JOIN menu_item m ON iap.menu_item_id = m.menu_item_id
+GROUP BY ro.order_id
+HAVING SUM(iap.quantity * m.menu_item_price) > 200
+ORDER BY ro.order_id;
+ 
+-- How many orders has General Grievous made?
+-- (GROUP BY and an agregation function)
 SELECT user_id, user_name, COUNT(order_id) as num_of_orders
 FROM rest_user NATURAL JOIN makes
 WHERE rest_user.user_name = 'General Grievous'
 GROUP BY user_id, user_name;
 
--- HOW MANY TABLES ARE RESERVED FOR THE 8TH OF APRIL 2025?
-
+-- How many tables are reserved for the 8th of April 2025?
+-- (Join of 2 tables)
 SELECT rest_table.table_id, COUNT(DISTINCT t_r.table_id) AS num_of_reserv_table
 FROM rest_table
 JOIN table_reservation t_r ON rest_table.table_id = t_r.table_id
 WHERE TRUNC(t_r.event_time) = TO_DATE('08.04.2025', 'DD.MM.YYYY')
 GROUP BY rest_table.table_id;
 
-
 -- Which menu items are alergen free?
+-- (Predicate EXISTS)
+SELECT DISTINCT mi.menu_item_name, mi.menu_item_price
+FROM menu_item mi
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM consists_of co
+	JOIN contains c ON co.ingredient_id = c.ingredient_id
+	WHERE co.menu_item_id = mi.menu_item_id
+);
 
--- How many orders were created in (day/week/month)?
-
--- Which menu item has been ordered the most?
+-- How many times has each menu item been ordered?
+-- (Join of 2 tables)
+SELECT mi.menu_item_id, mi.menu_item_name, SUM(iap.quantity) AS total_quantity_ordered
+FROM is_a_part_of iap
+JOIN menu_item mi ON iap.menu_item_id = mi.menu_item_id
+GROUP BY mi.menu_item_id, mi.menu_item_name
+ORDER BY total_quantity_ordered DESC;
